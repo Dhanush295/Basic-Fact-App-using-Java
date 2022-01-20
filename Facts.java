2a965a4629e042662687b6ce5e6f51f60f8a91cd
Dhanush295 <81304542+Dhanush295@users.noreply.github.com>
2022-01-20 19:17:51 +0530

    Create Facts.java

---
+++ Facts.java
@@ -0,0 +1,13 @@
+package com.example.newone;
+
+public class Facts {
+    public static String[] getData(){
+        return new String[]{
+            
+//            write your facts here
+          "Honeybees navigate by using the Sun as a compass.",
+
+        };
+
+    }
+}
